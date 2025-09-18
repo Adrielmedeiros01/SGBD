a6522309248f477d85e68f094b03ce8cc5f241c0
Adriel Medeiros <130292427+Adrielmedeiros01@users.noreply.github.com>
2025-09-17 21:49:27 -0300

    Update comandosSELECT.sql

--- comandosSELECT.sql
+++ comandosSELECT.sql
@@ -1,5 +1,11 @@
+-- Link BANCO DE DADOS EXEMPLO PARA CONSULTA: https://drive.google.com/file/d/1piXa1YKubaulDQDG_R5b0wBwwcfYQWP8/view
 
-
+/*
+Sintaxe:
+	'select' nomeColuna
+	'from' nomeTabela 
+	'where' condição
+*/
 use locadora;
 
 -- Trás todos os dados da tabela:
@@ -34,7 +40,6 @@ select concat(primeiro_nome, ' - ', ultimo_nome) as "Nome_completo" from ator;
 	 -- Criando uma nova coluna com apelido:
 select titulo, preco_da_locacao, preco_da_locacao*5 as preco_reais  from filme;
 
-
 select *  from filme where ano_de_lancamento = 2006 and classificacao =  "PG";
 
 -- Verificar a quantidade de rasultados da consulta:
@@ -46,6 +51,7 @@ select * from filme where preco_da_locacao >= 0.99 and preco_da_locacao <= 2.99;
 -- Especificar condições em que o valor deve estar contido com o BETWEEN: 
 select * from filme where preco_da_locacao between 0.99 and 2.00;
 select * from filme where preco_da_locacao not between 0.99 and 2.00;
-
 select * from filme where duracao_do_filme between 60 and 80;
+select data_de_aluguel from aluguel  where data_de_aluguel between "2005-05-24" and "2005-06-24" limit 10;
+
 
