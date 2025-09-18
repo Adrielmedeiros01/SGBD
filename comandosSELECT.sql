e728dddc665a0503f98c16dad84b74ed8a164f72
Adriel Medeiros <130292427+Adrielmedeiros01@users.noreply.github.com>
2025-09-17 21:39:52 -0300

    Create comandosSELECT.sql

---
+++ comandosSELECT.sql
@@ -0,0 +1,51 @@
+
+
+use locadora;
+
+-- Trás todos os dados da tabela:
+select * from cliente;
+
+-- Trás os dados apenas das colunas solicitadas:
+select primeiro_nome, ultimo_nome, email
+from cliente;
+
+-- Trás uma quantidade específica de linhas de acordo com a quantidade colocada no LIMIT:
+select *
+from filme
+limit 5;
+
+-- Força a eliminação de duplicações/repetições:
+
+select distinct primeiro_nome
+from ator;
+
+-- Criar apelidos para colunas e tabelas utilizando os AS:
+	-- Coluna:
+	-- select nomeColuna 'as' 'apelido' from tabela;
+	-- Tabela:
+	-- select c.nome from cliente as c;
+select primeiro_nome as nome, ultimo_nome as sobrenome 
+from cliente;
+
+-- Concatenar registros de colunas utilizando o CONCAT:
+select concat(primeiro_nome, ' - ', ultimo_nome) as "Nome_completo" from ator;
+
+-- Expressões aritméticas:
+	 -- Criando uma nova coluna com apelido:
+select titulo, preco_da_locacao, preco_da_locacao*5 as preco_reais  from filme;
+
+
+select *  from filme where ano_de_lancamento = 2006 and classificacao =  "PG";
+
+-- Verificar a quantidade de rasultados da consulta:
+select count(*)  from filme where ano_de_lancamento = 2006 and classificacao =  "PG";
+
+
+select * from filme where preco_da_locacao >= 0.99 and preco_da_locacao <= 2.99;
+
+-- Especificar condições em que o valor deve estar contido com o BETWEEN: 
+select * from filme where preco_da_locacao between 0.99 and 2.00;
+select * from filme where preco_da_locacao not between 0.99 and 2.00;
+
+select * from filme where duracao_do_filme between 60 and 80;
+
