c9f17b19425e20f100f3e9c94300c5d973e420e0
Adriel Medeiros <130292427+Adrielmedeiros01@users.noreply.github.com>
2025-10-22 21:26:05 -0300

    Add aggregate functions to FuncoesAgregacao.sql

--- FuncoesAgregacao.sql
+++ FuncoesAgregacao.sql
@@ -20,3 +20,12 @@ select max(data_de_aluguel) from aluguel;
 
 -- Exibe a quantidade de itens da tabela que começam com a letra A:
 select count(*) from ator where primeiro_nome like 'A%';
+
+-- Quantidade que possui x condição:
+select count(*) from filme where duracao_do_filme >= 90;
+
+-- Média de valores que possui a seuinte condição:
+select avg(preco_da_locacao) from filme where ano_de_lancamento = 2006 and classificacao = 'R';
+
+-- Soma dos valores de uma coluna, colocando esse valor em uma coluna apelidada de x, seguindo a seguinte condições:
+select sum(valor) from pagamento as total_pagamentos where data_de_pagamento between "2005-03-10" and "2005-11-30";
