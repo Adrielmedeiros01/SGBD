631f1ba6a8b34f4182b9ccbb85a16472d3d076f3
Adriel Medeiros <130292427+Adrielmedeiros01@users.noreply.github.com>
2025-11-05 19:22:53 -0300

    Create GROUP_BY.sql

---
+++ GROUP_BY.sql
@@ -0,0 +1,11 @@
+
+
+-- GROUP BY: 
+
+  -- Agrupamento de linhas que possuem o mesmo valor;
+  
+  -- É utilizado com as funções de agrgação;
+  
+  -- Fica posicionado após o WHERE e antes do ORDER BY;
+
+  --  SELECT, FROM, WHERE, GROUP BY, ORDER BY, LIMIT (Sequência de declaração);
