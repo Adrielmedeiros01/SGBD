72273577414a4fdd6f7cd7c99dabc1944826743d
Adriel Medeiros <130292427+Adrielmedeiros01@users.noreply.github.com>
2025-11-05 20:15:24 -0300

    Update GROUP_BY.sql

--- GROUP_BY.sql
+++ GROUP_BY.sql
@@ -1,11 +1,23 @@
-
-
 -- GROUP BY: 
 
   -- Agrupamento de linhas que possuem o mesmo valor;
-  
   -- É utilizado com as funções de agrgação;
-  
   -- Fica posicionado após o WHERE e antes do ORDER BY;
-
   --  SELECT, FROM, WHERE, GROUP BY, ORDER BY, LIMIT (Sequência de declaração);
+
+use locadora;
+
+-- Retorna a quantidade existente de acordo com a classificação:
+select classificacao, count(*) as filmes_Existentes  from filme
+group by classificacao;
+
+-- Retorna o valor total de cada "cliente" ordenados em ordem decrescente:
+select cliente_id, sum(valor) as compras_totais from pagamento 
+group by cliente_id
+order by compras_totais desc;
+
+-- Expoiler de como utilizar o join e associar os devidos valores das chaves primarias as chaves estrangeiras:
+select c.primeiro_nome, c.email, sum(p.valor) as compras_totais from pagamento p
+join cliente c on p.cliente_id = c.cliente_id
+group by p.cliente_id
+order by compras_totais desc;
