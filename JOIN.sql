58fe1b7c678d86612d3156ecea66d3d3c91b191f
Adriel Medeiros <130292427+Adrielmedeiros01@users.noreply.github.com>
2025-11-12 19:47:30 -0300

    Update JOIN.sql

--- JOIN.sql
+++ JOIN.sql
@@ -1,2 +1,47 @@
 
 -- Fazer uma busca extraindo dados de tabelas diferentes;
+/*
+# Consulta:
+  -Implcita: WHERE
+  -Explicita: JOIN
+*/
+
+/*
+# JOIN {
+SELECT p.nomeprod, c.nomecat
+FROM produto p
+JOIN catgoria c ON p.idcategoria = c.idcategoria
+}
+
+# LEFT JOIN {
+SELECT p.nomeprod, c.nomecat
+FROM produto p
+LEFT JOIN catgoria c ON p.idcategoria = c.idcategoria
+}
+
+
+# RIGHT JOIN {
+SELECT p.nomeprod, c.nomecat
+FROM produto p
+RIGHT JOIN catgoria c ON p.idcategoria = c.idcategoria
+}
+
+# FULL JOIN (Depende do SGBD){
+SELECT p.nomeprod, c.nomecat
+FROM produto p
+FULL JOIN catgoria c ON p.idcategoria = c.idcategoria
+}
+*/
+
+
+/*
+# (Junção Base)Produto cartesiano:
+    select * from tabela1, tabela2
+*/
+
+
+
+
+
+
+
